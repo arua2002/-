5534d7fcd51ac5c378a16d81af9b23d8dbecd9b1
ruslan <90987143+arua2002@users.noreply.github.com>
2023-03-12 15:31:47 +0500

    Add files via upload

---
+++ neuraset/neuraset/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace neuraset
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьПримерToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.начатьОбучениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.проверкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.очиститьToolStripMenuItem,
+            this.добавитьПримерToolStripMenuItem,
+            this.начатьОбучениеToolStripMenuItem,
+            this.проверкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(681, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.очиститьToolStripMenuItem.Text = "очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click);
+            // 
+            // добавитьПримерToolStripMenuItem
+            // 
+            this.добавитьПримерToolStripMenuItem.Name = "добавитьПримерToolStripMenuItem";
+            this.добавитьПримерToolStripMenuItem.Size = new System.Drawing.Size(115, 20);
+            this.добавитьПримерToolStripMenuItem.Text = "добавить пример";
+            this.добавитьПримерToolStripMenuItem.Click += new System.EventHandler(this.добавитьПримерToolStripMenuItem_Click);
+            // 
+            // начатьОбучениеToolStripMenuItem
+            // 
+            this.начатьОбучениеToolStripMenuItem.Name = "начатьОбучениеToolStripMenuItem";
+            this.начатьОбучениеToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.начатьОбучениеToolStripMenuItem.Text = "обучение";
+            this.начатьОбучениеToolStripMenuItem.Click += new System.EventHandler(this.начатьОбучениеToolStripMenuItem_Click);
+            // 
+            // проверкаToolStripMenuItem
+            // 
+            this.проверкаToolStripMenuItem.Name = "проверкаToolStripMenuItem";
+            this.проверкаToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.проверкаToolStripMenuItem.Text = "проверка";
+            this.проверкаToolStripMenuItem.Visible = false;
+            this.проверкаToolStripMenuItem.Click += new System.EventHandler(this.проверкаToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(340, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(329, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(160, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "назначить ожидаемый ответ и запустить\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ответ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(340, 139);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(329, 153);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(15, 327);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 434);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьПримерToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem начатьОбучениеToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem проверкаToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ neuraset/neuraset/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace neuraset
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Button[,] but = new Button[5, 5];
+        int [,] prim =new int[5, 25];
+        Random rnd = new Random();
+        double[] weight = new double[25];
+        bool b = false;
+        int pr = 0;
+        int otvet = 0;
+        double sigmoid(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+        double sum(int i)
+        {
+            double r1 = 0;
+            for (int j = 0; j < weight.Length; j++) r1 += prim[i, j] * weight[j];//y=x*w
+            return r1;
+        }
+        void learn()
+        {
+            int y = 0;
+            for (int i = 0; i < weight.Length; i++) weight[i] = rnd.NextDouble();
+            double err = 1;
+            while (err>0.01 || err<-0.01)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    err = otvet - sigmoid(sum(i));//err=output-sigmoid(y)
+                    for (int j = 0; j < weight.Length; j++) weight[j] += 0.0001 * (prim[i, j]*err); //это первый вариант
+                }
+                y++;
+            }
+            label2.Text = y.ToString();
+        }
+        double test(int[] x)
+        {
+            double r1 = 0;
+            for (int j = 0; j < weight.Length; j++) r1 += x[j] * weight[j];//y=x*w
+
+            return sigmoid(r1);
+        }
+        void clk(object sender, EventArgs e)
+        {
+            Button button = (sender as Button);
+            b = !b;
+            if (b) button.Text = "1";
+            else button.Text = "0";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = 50, y = 50;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    but[i, j] = new Button();
+                    but[i, j].Location = new Point(10+(i*x),30+(j*y));
+                    but[i, j].Text = "0";
+                    but[i, j].Size = new Size(50,50);
+                    but[i, j].Click += new EventHandler(clk);
+                    Controls.Add(but[i,j]);
+                }
+            }
+        }
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    but[i, j].Text = "0";
+                }
+            }
+        }
+        private void добавитьПримерToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int g = 0;
+            listBox1.Items.Clear();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    prim[pr,g] = Convert.ToInt32(but[i,j].Text);
+                    g++;
+                }
+            }
+            for (int i = 0; i < pr+1; i++)
+            {
+                string s = "";
+                for (int j = 0;j < 25; j++)
+                {
+                    s += prim[i, j].ToString();
+                }
+                listBox1.Items.Add(s);
+            }         
+            pr++;
+            if (pr > 4) добавитьПримерToolStripMenuItem.Enabled = false;
+        }
+        private void начатьОбучениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pr >= 4)
+            {
+                groupBox1.Visible = true;
+                label2.Text = "";
+            }
+            else label2.Text = "добавьте 5 примеров";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                otvet = Convert.ToInt32(textBox1.Text);
+                label2.Text = "";
+                learn();
+                проверкаToolStripMenuItem.Visible = true;
+            }
+            catch (Exception)
+            {
+                label2.Text = "введено не корректное значение";
+            }
+        }
+
+        private void проверкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int[] a = new int[25];
+            int g = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    a[g] = Convert.ToInt32(but[i, j].Text);
+                    g++;
+                }
+            }
+            double sw =  test(a);
+            if (sw > 0.9) label2.Text = "это с вероятностью" + (sw*100).ToString() + "%" + textBox1.Text;
+            else label2.Text = "это не " + textBox1.Text + "так как вероятность " + (sw * 100).ToString();
+            listBox2.Items.Add(sw);
+
+        }
+    }
+}
